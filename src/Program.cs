ab0e521658721e3be18c517f41bbd7f088196543
Gleb Vinogradov <g.a.vinogradov@gmail.com>
2016-02-21 17:57:42 +0600

    Texture shader

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
@@ -7,12 +8,36 @@ using OpenTK.Input;
 
 namespace hexandbox {
     class MyApplication {
+        
+
+        static Bitmap bitmap = new Bitmap("../data/Tileset_Hexagonal_PointyTop_60x52_60x80.png");
+        static int texture;
+
         [STAThread]
         public static void Main() {
             using (var game = new GameWindow()) {
                 game.Load += (sender, e) => {
                     // setup settings, load textures, sounds
                     game.VSync = VSyncMode.On;
+
+                    GL.ClearColor(Color.MidnightBlue);
+                    GL.Enable(EnableCap.Texture2D);
+
+                    GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+                    GL.GenTextures(1, out texture);
+                    GL.BindTexture(TextureTarget.Texture2D, texture);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+                    BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                        ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                    GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                        OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+                    bitmap.UnlockBits(data);
+
                 };
 
                 game.Resize += (sender, e) => {
@@ -27,23 +52,32 @@ namespace hexandbox {
                 };
 
                 game.RenderFrame += (sender, e) => {
-                    // render graphics
+                
+
+
+
+        // render graphics
                     GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
                     GL.MatrixMode(MatrixMode.Projection);
                     GL.LoadIdentity();
                     GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
 
-                    GL.Begin(PrimitiveType.Triangles);
+                    GL.Clear(ClearBufferMask.ColorBufferBit);
 
-                    GL.Color3(Color.MidnightBlue);
-                    GL.Vertex2(-1.0f, 1.0f);
-                    GL.Color3(Color.SpringGreen);
-                    GL.Vertex2(0.0f, -1.0f);
-                    GL.Color3(Color.Ivory);
-                    GL.Vertex2(1.0f, 1.0f);
+                    GL.MatrixMode(MatrixMode.Modelview);
+                    GL.LoadIdentity();
+                    GL.BindTexture(TextureTarget.Texture2D, texture);
+
+                    GL.Begin(PrimitiveType.Quads);
+
+                    GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-0.6f, -0.4f);
+                    GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(0.6f, -0.4f);
+                    GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(0.6f, 0.4f);
+                    GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(-0.6f, 0.4f);
 
                     GL.End();
+
 
                     game.SwapBuffers();
                 };
